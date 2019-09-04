32d25cf298e1f75c1a02e62d7494a8d0cdd1ce75
ashin <ashin@ashindeMacBook-Pro.local>
2019-09-04 23:59:18 +0800

    add hud has bag button and relative switch functionality.

---
+++ GameContent/UI/HUDUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameContent
+{
+    public class HUDUI : MonoBehaviour
+    {
+        public GameObject BagPanel = null;
+
+        public void SwitchBag()
+        {
+            if (BagPanel)
+            {
+                BagPanel.SetActive(!BagPanel.activeSelf);
+            }
+        }
+    }
+}
